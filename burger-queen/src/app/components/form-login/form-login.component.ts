913aa52c12bfeae2d5ae27ae7c20e40b1ebf105e
LizRojasEscobar <lizrojasescobar19@gmail.com>
2019-07-08 18:54:09 -0500

    agregando validaciones por el lado del servidor

--- burger-queen/src/app/components/form-login/form-login.component.ts
+++ burger-queen/src/app/components/form-login/form-login.component.ts
@@ -3,14 +3,16 @@ import { NgForm, FormGroup} from '@angular/forms'
 import {UserService } from 'src/app/services/user.service'
 import { UserModel} from 'src/app/model/model.model'
 import { Router } from '@angular/router';
+import Swal from 'sweetalert2'
+
 @Component({
   selector: 'app-form-login',
   templateUrl: './form-login.component.html',
   styleUrls: ['./form-login.component.css']
 })
 export class FormLoginComponent implements OnInit {
-
-  user : Object = {
+  
+  user:object= {
   email: '',
   password:''
 
@@ -25,14 +27,32 @@ export class FormLoginComponent implements OnInit {
   ngOnInit() {
    
     
+   
   }
  guardar(forma : FormGroup) {
-   console.log(forma.invalid);
-   
+   //if (forma.invalid){return;}
+  Swal.fire({
+    allowOutsideClick: false,
+    type: "info",
+    text: "espere por favor..."
+  });
+  Swal.showLoading()
   this.userservice.getToken(forma.value).subscribe(resp => {
+    Swal.close()
     this.router.navigateByUrl('/home')
-     console.log(resp) }),
-   (err =>console.log(err.HttpErrorResponse))
+     console.log(resp) },
+   (err )=>{
+    console.log(err.HttpErrorResponse)
+    Swal.fire({
+      allowOutsideClick: false,
+      type: "error",
+      text:"Error al autenticar..."
+  
+    });
+   });
+  
+    
+    
    
   //this.userservice.getProducts().subscribe(resp => console.log(resp) )
  }
