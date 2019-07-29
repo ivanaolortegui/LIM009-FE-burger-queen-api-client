5179049650a691f8c7eb6d51cba4c751ced1e88e
LizRojasEscobar <lizrojasescobar19@gmail.com>
2019-07-29 16:53:43 -0500

    agregando funcion de select para capturar datos

--- burger-queen/src/app/components/home/view/view-orders.component.ts
+++ burger-queen/src/app/components/home/view/view-orders.component.ts
@@ -12,14 +12,19 @@ import { orderResponse } from '../../../interface/orderResponse';
 export class ViewOrdersComponent implements OnInit {
  orders : orderResponse[];
   constructor(private orderService: OrdersService) { }
+  opcionSeleccionada: string  = 'pending';
+  verSeleccion: string        = '';
 
   ngOnInit() {
     this.orderService.getOrders().subscribe((respon:orderResponse[]) => {
       this.orders = respon// Obtener todos las ordenes
       console.log(respon);
     })
- 
-  
+ }
+ captureData(){
+  this.verSeleccion = this.opcionSeleccionada;
+  console.log(this.verSeleccion)
+
   }
 
 }
--- burger-queen/src/app/services/orders.service.ts
+++ burger-queen/src/app/services/orders.service.ts
@@ -65,7 +65,5 @@ deleteProduct(idx : number){
   }
 
 
-
-
 }
 
