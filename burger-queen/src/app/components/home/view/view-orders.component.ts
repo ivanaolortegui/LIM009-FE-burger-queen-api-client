7e1a54ee587e7c73eb68a31674836bf9a6309d60
ivanaolortegui <olorteguimor@gmail.com>
2019-07-31 12:32:30 -0500

    Agregando cronometro al sistema de ordenes ⏱⏱ ✨

--- burger-queen/src/app/components/home/view/view-orders.component.ts
+++ burger-queen/src/app/components/home/view/view-orders.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ReflectiveInjector } from "@angular/core";
 import { OrdersService } from "../../../services/orders.service";
 import { orderResponse } from "../../../interface/orderResponse";
 
@@ -12,30 +12,41 @@ export class ViewOrdersComponent implements OnInit {
   constructor(private orderService: OrdersService) {}
   opcionSeleccionada: string = "pending";
   verSeleccion: string = "";
-
-   options = ["pending", "canceled", "delivering", "delivered"];
-
+  options = ["pending", "canceled", "delivering", "delivered"];
+  min: number = 0;
+  seg: number = 0;
+  chronometer: any;
   ngOnInit() {
-   
     this.orderService.getOrders().subscribe((respon: orderResponse[]) => {
       this.orders = respon; // Obtener todos las ordenes
       console.log(respon);
     });
 
- 
+    this.chronometer = setInterval(() => {
+      this.seg++;
+      if (this.seg === 60) {
+        this.seg = 0;
+        this.min++;
+        if (this.min === 0) {
+          this.min = 0;
+        }
+      }
+    }, 1000);
   }
   captureData(item: any) {
     // Pasamos el valor seleccionado a la variable verSeleccion
     this.verSeleccion = this.opcionSeleccionada;
-   item.status = this.verSeleccion
+    item.status = this.verSeleccion;
 
-    console.log(this.verSeleccion);
+    console.log(this.opcionSeleccionada);
     console.log(item.id);
-    
+    if (this.verSeleccion === "delivered" || "canceled") {
+      clearInterval(this.chronometer);
+    }
 
-    this.orderService.putStatus(item, item.id).subscribe(resp => { //Envio objeto y id 
+    this.orderService.putStatus(item, item.id).subscribe(resp => {
+      //Envio objeto y id
       console.log(resp);
     });
   }
-  
 }
